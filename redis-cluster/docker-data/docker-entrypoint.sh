43247f08e43cd3dfc2c4b4cd5fa934ec2fef34da
root <root@ingematdevnodo01.ingemat.clientes.irontec.vpn>
2018-09-20 09:47:18 +0200

    Fixing Redis Cluster

--- redis-cluster/docker-data/docker-entrypoint.sh
+++ redis-cluster/docker-data/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'redis-cluster' ]; then
     sleep 10
-    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 10.168.80.101:7000 10.168.80.102:7001 10.168.80.103:7002 10.168.80.104:7003 10.168.80.105:7004 10.168.80.106:7005
+    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 10.80.254.5:7000 10.80.254.5:7001 10.80.254.5:7002 10.80.254.5:7003 10.80.254.5:7004 10.80.254.5:7005
     echo "DONE"
 else
   exec "$@"
